27696697c8bb1f9f47647f2a8c29b8683455cbe2
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-10 05:56:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if exists?(index) and position_taken?(board,index)
+  if exists?(index) and !position_taken?(board,index)
     return true
   end
   return false
